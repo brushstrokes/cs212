1dffe393b737e81150a29a951949b383dc1f2d1c
brushstrokes <gilryansalas@gmail.com>
2016-05-31 14:49:26 -0400

    Pushing c++ info

---
+++ LearnC++/Basics/variables.cpp
@@ -0,0 +1,50 @@
+/*
+ * Complete List
+ *
+ * Character types
+ *	char		Exactly one byte in size. At least 8 bits.
+ *	char16_t	Not smaller than char. At least 16 bits
+ *	char32_t	Not smaller than char16_t. At least 32 bits
+ *	wchar_t		Can represent the largest supported char set
+ *
+ * Integer Types (signed)
+ * 	signed char 	 Same size as char. At least 8 bits
+ *	signed short int Not smaller than char. At least 16 bits
+ *	signed int	 Not smaller than short. At least 16 bits
+ *	signed long int  Not smaller than int. At least 32 bits
+ *	signed long long int not smaller thant long. At least 64 bits
+ *
+ * Integer Types (unsigned)
+ *	unsigned char
+ *	unsigned short int
+ *	unsigned int		Same size as their signed counterpart
+ *	unsigned long int
+ *	unsigned long long int
+ *
+ * Floating-point types
+ *	float
+ *	double		Precision not less than float
+ *	long double	Precision not less than double
+ * 
+ * Boolean Type
+ * 	bool
+ * Void type
+ * 	void
+ * Null pointer		No storage
+ * 	decltype(nullptr)
+ *
+ * */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+}
+
+
+
